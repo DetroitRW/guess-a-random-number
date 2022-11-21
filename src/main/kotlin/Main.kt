275729a89ccf5620e2f3625590c1ce07b979de33
Ilya Brant <detroitib31@gmail.com>
2022-11-21 07:07:07 +0600

    Написал сканнер и текст

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,8 @@ import java.util.*
 fun main(args: Array<String>) {
 
     val n = Random(System.nanoTime()).nextInt(0, 100)
+
+    val reader = Scanner(System.`in`)
+
+    println("Отгадайте число от 1 до 100 за 10 попыток")
 }
