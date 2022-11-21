5936acb72d5aede802f2b71c43aea87e47f0b913
Ilya Brant <detroitib31@gmail.com>
2022-11-21 07:07:59 +0600

    Написал алгоритм для цикла

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,4 +7,23 @@ fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
     println("Отгадайте число от 1 до 100 за 10 попыток")
+
+    var i = 1
+
+    while (i <= 10) {
+        print("$i-я попытка: ")
+        val a = reader.nextInt()
+        if (a > n) {
+            println("Много")
+        } else if (a < n) {
+            println("Мало")
+        } else {
+            println("Угадано!")
+            break
+        }
+        i += 1
+        }
+    if (i == 11) {
+        print("Вы не угадали. Было загадано: $n")
+    }
 }
